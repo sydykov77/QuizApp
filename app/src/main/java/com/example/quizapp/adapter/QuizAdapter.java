d332097ab61d320196f50481da3059ebb050b2a1
sydykov77 <erjansydykov77@gmail.com>
2020-09-28 17:34:30 +0600

    HW #2

---
+++ app/src/main/java/com/example/quizapp/adapter/QuizAdapter.java
@@ -0,0 +1,98 @@
+package com.example.quizapp.adapter;
+
+import android.annotation.SuppressLint;
+import android.os.Build;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.quizapp.R;
+import com.example.quizapp.databinding.ItemQuestionBinding;
+import com.example.quizapp.interfaces.OnButtonAnswerClick;
+import com.example.quizapp.models.QuizModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuizAdapter extends RecyclerView.Adapter<QuizAdapter.ViewHolder> {
+    private List<QuizModel> quizModels = new ArrayList<>();
+    private OnButtonAnswerClick answerClick;
+
+    public QuizAdapter(OnButtonAnswerClick viewModel) {
+        this.answerClick = viewModel;
+    }
+
+    public void setQuestions(List<QuizModel> quizModels) {
+        this.quizModels = quizModels;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_question, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.onBind(quizModels.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return quizModels.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnTouchListener {
+        private ItemQuestionBinding item;
+        @SuppressLint("ClickableViewAccessibility")
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            item = ItemQuestionBinding.bind(itemView);
+            item.button1.setOnTouchListener(this);
+            item.button2.setOnTouchListener(this);
+            item.button3.setOnTouchListener(this);
+            item.button4.setOnTouchListener(this);
+            item.type2Button.setOnTouchListener(this);
+            item.type2Button1.setOnTouchListener(this);
+        }
+
+        public void onBind(QuizModel quizModel){
+            quizModel.setId(getAdapterPosition());
+            item.setModel(quizModel);
+            item.setHandlers(answerClick);
+            item.button1.setBackgroundResource(R.drawable.item_button_4);
+            item.button2.setBackgroundResource(R.drawable.item_button_4);
+            item.button3.setBackgroundResource(R.drawable.item_button_4);
+            item.button4.setBackgroundResource(R.drawable.item_button_4);
+            item.type2Button.setBackgroundResource(R.drawable.item_button_4);
+            item.type2Button1.setBackgroundResource(R.drawable.item_button_4);
+        }
+
+
+
+        @SuppressLint("ClickableViewAccessibility")
+        @RequiresApi(api = Build.VERSION_CODES.M)
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            Button button = (Button) v;
+            switch(event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    button.setBackgroundResource(R.drawable.frame);
+                    button.setTextAppearance(R.style.item_btn_text);
+                    return false; // if you want to handle the touch event
+                case MotionEvent.ACTION_UP:
+                    button.setBackgroundResource(R.drawable.item_button_4);
+                    button.setTextAppearance(R.style.item_btn2_text);
+                    return false; // if you want to handle the touch event
+            }
+            return false;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/quizapp/ui/questions_activity/QuestionsActivity.java
@@ -0,0 +1,89 @@
+package com.example.quizapp.ui.questions_activity;
+
+import android.annotation.SuppressLint;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.example.quizapp.R;
+import com.example.quizapp.adapter.QuizAdapter;
+import com.example.quizapp.castom_view.CustomGridLayoutManager;
+import com.example.quizapp.databinding.ActivityQuestionsBinding;
+import com.example.quizapp.interfaces.OnButtonAnswerClick;
+
+public class QuestionsActivity extends AppCompatActivity implements OnButtonAnswerClick {
+    public static final String RESULT_QUESTIONS_AMOUNT_KEY = "RESULT_QUESTIONS_AMOUNT_KEY";
+    private int questionsAmount;
+    private ActivityQuestionsBinding activityQuestionsBinding;
+    private QuizAdapter quizAdapter;
+    private QuestionsViewModel mViewModel;
+    private CustomGridLayoutManager customGridLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.Theme_AppCompat_Light_NoActionBar);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questions);
+
+        init();
+        setArg();
+        setListener();
+    }
+
+    private void setListener() {
+        activityQuestionsBinding.path.setOnClickListener(v -> finish());
+    }
+
+    private void setArg() {
+        mViewModel.setAmountQuestions(questionsAmount);
+        activityQuestionsBinding.recyclerview.setAdapter(quizAdapter);
+        activityQuestionsBinding.recyclerview.setLayoutManager(customGridLayoutManager);
+        customGridLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        activityQuestionsBinding.progressBarQuestionActivity.setMax(questionsAmount);
+    }
+
+    private void init() {
+        mViewModel = ViewModelProviders.of(this).get(QuestionsViewModel.class);
+        activityQuestionsBinding = DataBindingUtil.setContentView(this, R.layout.activity_questions);
+        quizAdapter = new QuizAdapter(this);
+        if (getIntent() != null)
+            questionsAmount = getIntent().getIntExtra(RESULT_QUESTIONS_AMOUNT_KEY, 10);
+        customGridLayoutManager = new CustomGridLayoutManager(this);
+    }
+
+
+    @SuppressLint("ClickableViewAccessibility")
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onClick(View view, int positionQuestion, int positionAnswer) {
+        int result = mViewModel.onButtonClick(positionQuestion, positionAnswer);
+        switch (result) {
+            case QuestionsViewModel.CORRECT_ANSWER:
+                view.setBackgroundResource(R.drawable.item_button_2);
+                break;
+
+            case QuestionsViewModel.CORRECT_ANSWER_AND_AND_FINAL_ANSWER:
+                view.setBackgroundResource(R.drawable.item_button_2);
+                finish();
+                break;
+
+            case QuestionsViewModel.WRONG_ANSWER:
+                view.setBackgroundResource(R.drawable.item_button_3);
+                break;
+
+            case QuestionsViewModel.WRONG_ANSWER_AND_AND_FINAL_ANSWER:
+                view.setBackgroundResource(R.drawable.item_button_3);
+                finish();
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + result);
+        }
+    }
+}
